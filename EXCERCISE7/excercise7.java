1912b52c83a0259657d5aac23d65fad4eec888db
Amayavinod <74695779+Amayavinod@users.noreply.github.com>
2021-12-20 14:39:34 +0530

    Update excercise7.java

--- EXCERCISE7/excercise7.java
+++ EXCERCISE7/excercise7.java
@@ -1,1 +1,22 @@
+package com.example.myapplication;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void clickMethod(View view)
+    {
+        Toast.makeText(this,"welcome",Toast.LENGTH_SHORT).show();
+    }
+}
